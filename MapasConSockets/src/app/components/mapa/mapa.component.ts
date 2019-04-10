c72eb33b929481cca4ccb2121157ca0075eccd4f
bryancdn969 <bryan.cdn.20@gmail.com>
2019-04-10 01:21:05 -0500

    View map
    Correcion de mapas

--- MapasConSockets/src/app/components/mapa/mapa.component.ts
+++ MapasConSockets/src/app/components/mapa/mapa.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-mapa',
@@ -7,8 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapaComponent implements OnInit {
 
+  @ViewChild('map') mapElement: ElementRef;
+  map: google.maps.Map;
+
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+
+    this.cargarMapa();
+  }
+
+  cargarMapa() {
+
+    const latlng = new google.maps.LatLng(37.784669, -122.395936);
+
+    const mapaOptions = {
+      center: latlng,
+      zoom: 13,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapaOptions);
+  }
 
 }
